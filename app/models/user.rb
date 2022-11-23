38f0765eb56df28fc56e05b15a6916ceb0c29e79
ikun2019 <0215.miyatto@gmail.com>
2022-11-23 23:56:16 +0900

    nameとpasswordのバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,19 @@
 class User < ApplicationRecord
+    has_secure_password
+
+    validates   :name, presence: true,  # 入力必須
+                        length: {
+                            maximum: 30,
+                            allow_blank: true
+                        }
+    VALID_PASSWORD_REGEX = /\A[\w\-]+\z/
+    validates :password, presence: true,
+                            length: {
+                                minimum: 8,
+                                allow_blank: true
+                            },
+                            format: {
+                            with: VALID_PASSWORD_REGEX
+                            },
+                            allow_nil: true
 end
